542bde7396673315ae968087b4ffe1f0f466c6a6
plwtx <h.lenny933@gmail.com>
2025-01-11 03:21:32 +0100

    0.53 | Input field fixes

--- src/components/taskInputField.jsx
+++ src/components/taskInputField.jsx
@@ -1,15 +1,16 @@
-import { TbSteam } from "react-icons/tb";
+import { TbSteam, TbArrowBigUpLinesFilled } from "react-icons/tb";
 
 import "../i18n";
 import { useTranslation } from "react-i18next";
 
-const TaskInputField = ({ task, onTaskChange, onKeyPress }) => {
+const TaskInputField = ({ task, onTaskChange, handleAddTask, onKeyPress }) => {
   const { t } = useTranslation();
   return (
-    <div className="w-full bg-bgPrimary py-9">
-      <div className="border-t border-secondary bg-bgPrimary">
+    <div className="my-3 w-full bg-bgPrimary py-9">
+      {/* Task input field */}
+      <div className="mx-3 rounded-lg border border-b-2 border-secondary45 bg-bgPrimary bg-white p-3 shadow-sm shadow-text">
         <div className="flex flex-row justify-between">
-          <div className="flex flex-grow items-center gap-x-1 text-secondary">
+          <div className="flex flex-grow items-center gap-x-6 px-3 text-secondary">
             <TbSteam />
             <input
               onKeyDown={onKeyPress}
@@ -22,6 +23,11 @@ const TaskInputField = ({ task, onTaskChange, onKeyPress }) => {
               autoComplete="off"
               className="peer w-full appearance-none border-opacity-90 bg-transparent py-1 font-Outfit tracking-widest text-primary outline-none placeholder:tracking-tight placeholder:text-secondary placeholder:opacity-90 focus:ring-0"
             />
+
+            {/* Add button */}
+            {/* <button className="bg-clockText rounded-lg p-2">
+              <TbArrowBigUpLinesFilled className="fill-secondary45" />
+            </button> */}
           </div>
         </div>
       </div>
